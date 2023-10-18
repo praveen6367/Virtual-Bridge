de360db8f804ec69806a62b83c3f47312eab2890
Praveen Suthar <officialpraveensuthar@gmail.com>
2023-10-18 09:19:17 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import  express  from "express";
+
+const app = express() ;
+const port = 4000 ;  
+
+app.listen(port, ()=>{
+    console.log("the server has been started on the server 4000") ; 
+}) ; 
